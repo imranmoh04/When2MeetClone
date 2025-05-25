bcc65676057b8e464e2c5d32a3c35bd6bc4c91fa
imranmoh04 <mohidimran0419@gmail.com>
2025-05-25 13:30:18 -0400

    Delete Final-Exam directory

--- Final-Exam/flask_app/static/js/event_select.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector(".event-create").addEventListener("click", function () {
-      window.location.href = "/event_create";
-    });
-
-    document.querySelector(".event-join").addEventListener("click", function () {
-      window.location.href = "/event_join";
-    });
-});
